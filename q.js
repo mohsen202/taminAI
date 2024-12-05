3d54ac942f4b1297d165ab3d91058b2cc1effb43
mohsen202 <38821347+mohsen202@users.noreply.github.com>
2024-12-05 07:34:18 +0330

    Delete q.js

--- q.js
+++
@@ -1,8 +0,0 @@
-import { Client } from "@gradio/client";
-
-const client = await Client.connect("https://71bf507d2731ca29d7.gradio.live/");
-const result = await client.predict("/predict", { 		
-		input_text: "Hello!!", 
-});
-
-console.log(result.data);
